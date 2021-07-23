57db8b79a72c027e85cba4fae5a5d967f6aed372
TIAGO SILVA <frost.tiago@gmail.com>
2021-07-23 12:26:49 -0300

    Adicionar arquivos de projeto.

---
+++ AppGuiaDevTerceiro/AppGuiaDevTerceiro/MainPage.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppGuiaDevTerceiro
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Detail = new NavigationPage(new NovaPasta1.Inicial());
+        }
+
+        private void Button_Clicked_Contato(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage(new NovaPasta1.Contato());
+                IsPresented = false;
+
+            } catch(Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private void Button_Clicked_Inicial(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage(new NovaPasta1.Inicial());
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private void Button_Clicked_Bd(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage(new NovaPasta1.BancoDados());
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private void Button_Clicked_ProgWeb(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage(new NovaPasta1.ProgramacaoWeb());
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private void Button_Clicked_Dd(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage(new NovaPasta1.DesignDigital());
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private void Button_Clicked_ProgMobile(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage(new NovaPasta1.ProgramacaoMobile());
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private void Button_Clicked_So(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage(new NovaPasta1.SistemasOperacionais());
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private void Button_Clicked_Rc(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage(new NovaPasta1.RedesComputadores());
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private void Button_Clicked_Sd(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage(new NovaPasta1.SegurancaDigital());
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+}
---
+++ AppGuiaDevTerceiro/AppGuiaDevTerceiro/NovaPasta1/Contato.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.OpenWhatsApp;
+using Xamarin.Essentials;
+
+namespace AppGuiaDevTerceiro.NovaPasta1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Contato : ContentPage
+    {
+        public Contato()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked_Ligar(object sender, EventArgs e)
+        {
+            try
+            {
+                Launcher.OpenAsync("tel:+551436223566");                
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private void Button_Clicked_Email(object sender, EventArgs e)
+        {
+            try
+            {
+                Launcher.OpenAsync("mailto:e070acad@etec.sp.gov.br");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked_WhatsApp(object sender, EventArgs e)
+        {
+            try
+            {
+                Chat.Open("+551436223566");
+            }
+            catch (Exception ex)
+            {
+                bool abrir_discador = await DisplayAlert("Ops, Não foi possível abrir o WhatsApp", "Abrir o Discadador?", "Sim", "Agora Não");
+
+                if(abrir_discador)
+                    await Launcher.OpenAsync("tel:+551436223566");
+            }
+
+        }
+    }
+}
